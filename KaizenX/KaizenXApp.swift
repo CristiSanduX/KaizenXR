0df1e3987d4696cba923bec4ca90b3ab78a7eec7
Cristi Sandu <cristi.sandu@csx.ro>
2023-10-10 01:35:15 +0300

    Initialize KaizenX project

---
+++ KaizenX/KaizenXApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaizenXApp.swift
+//  KaizenX
+//
+//  Created by Cristi Sandu on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct KaizenXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
